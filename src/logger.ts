05f7701cca13b3ea611baf7cd87c4214c8bc63f4
Andy Gifford <ajgifford@users.noreply.github.com>
2025-04-17 16:57:44 -0500

    Change how the project is being exported

---
+++ src/logger.ts
@@ -0,0 +1,3 @@
+export * from './logger/logger';
+export * from './logger/loggerModel';
+export * from './logger/loggerUtil';
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export * from './types/contentTypes';
+export * from './types/discoverAndSearchTypes';
+export * from './types/movieTypes';
+export * from './types/notificationTypes';
+export * from './types/showTypes';
