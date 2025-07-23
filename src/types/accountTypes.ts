d6bf3df386a65ac82232137dcba5d92aba4e6239
Andy Gifford <ajgifford@users.noreply.github.com>
2025-07-23 14:33:10 -0500

    Add account reference row

--- src/types/accountTypes.ts
+++ src/types/accountTypes.ts
@@ -1,15 +1,26 @@
-import { Account } from '@ajgifford/keepwatching-types';
+import { Account, AccountReference } from '@ajgifford/keepwatching-types';
 import { RowDataPacket } from 'mysql2';
 
-export interface AccountRow extends RowDataPacket {
+export interface AccountReferenceRow extends RowDataPacket {
   account_id: number;
   account_name: string;
   email: string;
+}
+
+export interface AccountRow extends AccountReferenceRow {
   uid: string;
   image: string;
   default_profile_id: number;
   created_at: Date;
   updated_at: Date;
+}
+
+export function transformAccountReferenceRow(row: AccountReferenceRow): AccountReference {
+  return {
+    id: row.account_id,
+    name: row.account_name,
+    email: row.email,
+  };
 }
 
 export function transformAccountRow(row: AccountRow): Account {
